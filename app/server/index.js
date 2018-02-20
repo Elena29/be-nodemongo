717701dc805538c922db52fee83bf9ef787464ff
olenamelashchenko <interest29@gmail.com>
2018-02-20 23:00:31 +0100

    Setup router

--- app/server/index.js
+++ app/server/index.js
@@ -9,16 +9,16 @@ const flash = require('connect-flash');
 //Initialize application
 const app = express();
 
+// Load Routes
+const receipts = require('../routes/receipts');
+const users = require('../routes/users');
+
 // Connect to mongoose
 mongoose.connect('mongodb://mongo:27017')
   .then(() => {
       console.log('MongoDb Connected... ')
   })
   .catch(err => console.log(err));
-
-// Load Receipt Model
-require('../models/receipt');
-const Receipt = mongoose.model('receipt');
 
 
 // Handlebars Middleware
@@ -62,77 +62,10 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.get('/receipt', (req, res) => {
-    Receipt.find({})
-        .sort({date: 'desc'})
-        .then(receipts => {
-            res.render('receipt/index', {
-                receipts
-            });
+// Use routes
+app.use('/receipt', receipts);
+app.use('/users', users);
 
-        });
-});
-
-app.get('/receipt/add', (req, res) => {
-    res.render('receipt/add');
-});
-
-app.get('/receipt/edit/:id', (req, res) => {
-    Receipt.findOne({
-        _id: req.params.id
-    })
-        .then(receipt => {
-            res.render('receipt/edit', {receipt})
-        });
-});
-
-app.post('/receipt', (req, res) => {
-    let errors = [];
-    if (!req.body.title) {
-        errors.push({text: 'Please add a title'});
-    }
-    if (!req.body.details) {
-        errors.push({text: 'Please add some details'});
-    }
-
-    if (errors.length > 0) {
-        res.render('receipt/add', {errors, title: req.body.title, details: req.body.details});
-    } else {
-        const newReceipt = {
-            title: req.body.title,
-            details: req.body.details
-        };
-        new Receipt(newReceipt)
-            .save()
-            .then(receipt => {
-              req.flash('success_msg', 'Receipt added');
-              res.redirect('/receipt');
-            })
-    }
-});
-
-app.put('/receipt/:id', (req, res) => {
-    Receipt.findOne({
-        _id: req.params.id
-    })
-        .then(receipt => {
-          receipt.title = req.body.title;
-          receipt.details = req.body.details;
-          receipt.save()
-                .then(receipt => {
-                    req.flash('success_msg', 'Receipt updated');
-                    res.redirect('/receipt');
-                });
-        })
-});
-
-app.delete('/receipt/:id', (req, res) => {
-  Receipt.remove({_id: req.params.id})
-    .then(() => {
-      req.flash('success_msg', 'Receipt removed');
-      res.redirect('/receipt')
-    })
-});
 
 const port = 3000;
 app.listen(port, () => {
