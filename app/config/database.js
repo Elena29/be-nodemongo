0aae6059314e429a518d4f7797d2eeccb06c4bd3
olenamelashchenko <interest29@gmail.com>
2018-02-21 22:40:20 +0100

    Fix password to DB

--- app/config/database.js
+++ app/config/database.js
@@ -1,6 +1,6 @@
 let url;
 if (process.env.NODE_ENV === 'production') {
-  url = 'mongodb://admin:root@ds245228.mlab.com:45228/receipts-prod';
+  url = 'mongodb://<dbusername>:<dbpass>@ds245228.mlab.com:45228/receipts-prod';
 } else {
   url = 'mongodb://mongo:27017';
 }
